3875e533a9829b423a7c6f85cab5ebe78382d045
Kali <manishpc@localhost>
2026-01-12 20:48:24 +0530

    all movies

--- movie_view/settings.py
+++ movie_view/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ["*", "0.0.0.0"]
+ALLOWED_HOSTS = ["*", "0.0.0.0", "movies-iwiu.onrender.com"]
 
 # Application definition
 
